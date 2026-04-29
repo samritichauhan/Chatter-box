07de1c50de508173c0185034c0ed35c2195f0974
samriti <samritichauhan000@gamil.com>
2026-04-30 01:16:19 +0530

    Remove duplicate schema indexes from User model

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -37,9 +37,6 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.index({ email: 1 });
-userSchema.index({ username: 1 });
-
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 12);
