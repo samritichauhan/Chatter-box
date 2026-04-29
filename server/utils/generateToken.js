428586a00751cf4bcffdde5fd024b6763eb920d5
samriti <samritichauhan000@gamil.com>
2026-04-30 01:43:58 +0530

    Fix cookie SameSite policy for cross-origin requests

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -8,8 +8,8 @@ const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
-    sameSite: "strict",
-    secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+    secure: true,
   });
 
   return token;
